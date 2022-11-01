4867d3460a6689ca359d6399067b3e42ac99d182
DenisaCotuna <73788276+DenisaCotuna@users.noreply.github.com>
2022-11-02 01:29:22 +0200

    until ex 6 from 3.2 + print

--- src/ro/uvt/p3/io/OutputDevice.java
+++ src/ro/uvt/p3/io/OutputDevice.java
@@ -1,9 +1,15 @@
 package ro.uvt.p3.io;
 
+import ro.uvt.p3.Fruit;
+
 public class OutputDevice {
     public void writeMessage(Object msg)
     {
-
         System.out.println(msg);
+    }
+    public static void printFruits(Fruit[] fruits)
+    {
+        for(Fruit f:fruits)
+            System.out.println(f);
     }
 }
